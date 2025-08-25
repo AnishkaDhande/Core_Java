8131f222dc808bf66118066f39d96cb787087d6e
Anishka Dhande <anishkadhande8@gmail.com>
2025-08-25 19:44:24 +0530

    Add files via upload

---
+++ Numbers/Replacedigit.java
@@ -0,0 +1,16 @@
+public class Replacedigit {
+    public ststic void main(string args[])
+    {
+      int a=6392;
+
+      //step 1- convert Interger to string
+      String s=String.valueOf(a);
+
+      //step 2- use function of String to replace
+
+      String c=replace()
+
+
+    }
+    
+}
