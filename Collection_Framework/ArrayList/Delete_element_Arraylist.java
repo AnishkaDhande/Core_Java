6407334a7dcd1d290effee96e707dc0c41827c9e
Anishka Dhande <anishkadhande8@gmail.com>
2026-03-13 23:04:33 +0530

    Add files via upload

---
+++ Collection_Framework/ArrayList/Delete_element_Arraylist.java
@@ -0,0 +1,32 @@
+//write a program to delete value from particular index
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Delete_element_Arraylist{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of an Array");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter "+n+" elements in Array");
+        for(int i=0;i<=a.length-1;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        //covert array to arraylist 
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<=a.length-1;i++)
+        {
+            list.add(a[i]);
+        }
+        System.out.println(list);
+        System.out.println("Enter index of element");
+        int b=sc.nextInt();
+        list.remove(b);
+        System.out.println(list);
+
+    
+}
+    
+}
